aab9173181387f7cb3d376e4f9b92d9e8b720e72
Brandon Garzon <brandon.garzon@esums.tech>
2025-03-18 19:14:24 +0000

    finished adding images and the css

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.hellotime, now="hellotime"),
+    path("", views.hellotime, name="hellotime"),
     path("screenprint", views.screenprint, name="screenprint")
 ]
--- core/views.py
+++ core/views.py
@@ -4,7 +4,7 @@ import datetime
 
 def hellotime(request):
     now = datetime.datetime.now()
-    return HttpResponse(f"<h1>hi im big c</h1> <p>rn its {now}</p>")
+    return HttpResponse(f"<h1>salutations</h1> <p>rn its {now}</p>")
 
 def screenprint(request):
     return render(request, "core/screenprint.html")
